b778790cb9009ea229bf18421ddfa2534bb0d8e9
wanjirukinyira-alt <wanjirukinyira@gmail.com>
2026-08-04 12:26:03 +0300

    added dockerfile and nginx.conf and made changes on vite.config,js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        menu: resolve(__dirname, "menu.html"),
+        contact: resolve(__dirname, "contact.html"),
+        about: resolve(__dirname, "about.html"),
+        order: resolve(__dirname, "orderpage.html"),
+      },
+    },
+  },
+});
