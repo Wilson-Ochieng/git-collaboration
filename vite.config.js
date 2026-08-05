35ff33339350b526ba47550bae0a50f02daac0eb
MervynMaina <mervynk020@gmail.com>
2026-08-05 11:39:30 +0300

    docker

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        about: resolve(__dirname, 'about.html'),
+        order: resolve(__dirname, 'orderpage.html'),
+        // add any other real .html files ls shows you
+      },
+    },
+  },
 })
